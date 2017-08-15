93feee191c28bc4bc282243b1744dd35add9d909
Martino Visintin <martino.visintin@ovoenergy.com>
2017-08-15 15:08:38 +0100

    init cmt

---
+++ src/kafka_avro_confluent/core.clj
@@ -0,0 +1,6 @@
+(ns kafka-avro-confluent.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/kafka_avro_confluent/core_test.clj
@@ -0,0 +1,7 @@
+(ns kafka-avro-confluent.core-test
+  (:require [clojure.test :refer :all]
+            [kafka-avro-confluent.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
