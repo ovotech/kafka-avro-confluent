366e521c1cc5859f72d88848dc7499cf90221e90
Martino Visintin <martino.visintin@ovoenergy.com>
2017-08-16 10:07:45 +0100

    match kafka client version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.kafka/kafka-clients "0.11.0.0-cp1"]
+                 [org.apache.kafka/kafka-clients "0.10.2.1"]
                  [com.damballa/abracad "0.4.13"]
                  [cheshire "5.7.1"]
                  [clj-http "3.6.1"]]
