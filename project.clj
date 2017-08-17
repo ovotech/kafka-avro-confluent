90d2a08620d8577cb441c73c316ec835dd2b33a2
Martino Visintin <vise890@gmail.com>
2017-08-17 16:06:53 +0100

    bump vers

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/kafka-avro-confluent "0.3.0"
+(defproject ovotech/kafka-avro-confluent "0.4.0"
   :description "An Avro Kafka De/Serializer lib that works with Confluent's Schema Registry"
 
   :url "http://github.com/ovotech/kafka-avro-confluent"
