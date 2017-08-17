6868d0aa08513a3bf249ff8a295d48eb0dadf9b9
Bill Hedworth <bill.hedworth@ovoenergy.com>
2017-08-17 14:43:26 +0100

    fix aot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/kafka-avro-confluent "0.2.0"
+(defproject ovotech/kafka-avro-confluent "0.3.0"
   :description "An Avro Kafka De/Serializer lib that works with Confluent's Schema Registry"
 
   :url "http://github.com/ovotech/kafka-avro-confluent"
@@ -16,6 +16,8 @@
                   :exclusions [org.scala-lang/scala-library]] ]
 
   :repositories {"confluent" "http://packages.confluent.io/maven"}
+
+  :aot :all
 
   :profiles {:uberjar {:aot :all}
              :dev {:resource-paths ["test/resources"]
