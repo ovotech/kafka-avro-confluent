e15392854735d6f7a7c13dae4e175e7aa657a9ed
Alistair Dutton <alistair.dutton@ovoenergy.com>
2017-09-25 10:47:22 +0100

    Version bump to 0.8.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/kafka-avro-confluent "0.8.1"
+(defproject ovotech/kafka-avro-confluent "0.8.2"
 
   :description "An Avro Kafka De/Serializer lib that works with Confluent's Schema Registry"
 
