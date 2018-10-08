fda48919a1a0d4ba1c65e59d81a1cefad12da31b
Martino Visintin <martino.visintin@ovoenergy.com>
2018-10-08 12:06:45 +0100

    update deps

--- project.clj
+++ project.clj
@@ -10,13 +10,13 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.memoize "0.7.1"]
                  [com.damballa/abracad "0.4.13"]
-                 [cheshire "5.8.0"]
+                 [cheshire "5.8.1"]
 
-                 [clj-http "3.8.0"]
+                 [clj-http "3.9.1"]
 
-                 [org.apache.kafka/kafka-clients "1.0.1"
+                 [org.apache.kafka/kafka-clients "1.1.1"
                   :exclusions [org.scala-lang/scala-library]]
-                 [org.clojure/tools.logging "0.4.0"]]
+                 [org.clojure/tools.logging "0.4.1"]]
 
   :aot [kafka-avro-confluent.serializers
         kafka-avro-confluent.deserializers
@@ -25,7 +25,7 @@
 
   :repositories {"confluent" "https://packages.confluent.io/maven"}
 
-  :profiles {:dev {:dependencies   [[vise890/zookareg "1.0.2-2"]
+  :profiles {:dev {:dependencies   [[vise890/zookareg "1.1.1-1"]
                                     [ch.qos.logback/logback-classic "1.2.3"]
                                     [ch.qos.logback/logback-core "1.2.3"]]
                    :resource-paths ["dev/resources" "test/resources"]}
