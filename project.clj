59fd71c266df696c47cb64f695c8ad54b59a92da
Alistair Dutton <alistair.dutton@kaluza.com>
2020-09-04 08:52:37 +0100

    Only AOT in dev profile to avoid namespaces from libs like cheshire being hardbaked into the library and thus confusing the classloader for downstream libraries.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/kafka-avro-confluent "2.1.0-5"
+(defproject ovotech/kafka-avro-confluent "2.1.0-6"
 
   :description "An Avro Kafka De/Serializer lib that works with Confluent's Schema Registry"
 
@@ -18,18 +18,17 @@
                   :exclusions [org.scala-lang/scala-library]]
                  [org.clojure/tools.logging "0.4.1"]]
 
-  :aot [kafka-avro-confluent.serializers
-        kafka-avro-confluent.deserializers
-        kafka-avro-confluent.v2.serializer
-        kafka-avro-confluent.v2.deserializer]
-
   :repositories {"confluent" "https://packages.confluent.io/maven"}
 
   :profiles {:dev {:dependencies   [[vise890/zookareg "2.1.0-1"]
                                     [ch.qos.logback/logback-classic "1.2.3"]
                                     [ch.qos.logback/logback-core "1.2.3"]]
-                   :resource-paths ["dev/resources" "test/resources"]}
-             :ci {:deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                                    :username      :env ;; LEIN_USERNAME
-                                                    :password      :env ;; LEIN_PASSWORD
-                                                    :sign-releases false}]]}})
+                   :resource-paths ["dev/resources" "test/resources"]
+                   :aot            [kafka-avro-confluent.serializers
+                                    kafka-avro-confluent.deserializers
+                                    kafka-avro-confluent.v2.serializer
+                                    kafka-avro-confluent.v2.deserializer]}
+             :ci  {:deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                                     :username      :env ;; LEIN_USERNAME
+                                                     :password      :env ;; LEIN_PASSWORD
+                                                     :sign-releases false}]]}})
