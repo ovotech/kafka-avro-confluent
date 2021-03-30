8533edf83ca9fd84648e70eb59a08811856a99bd
Bill Hedworth <bill.hedworth@gmail.com>
2021-03-30 18:35:12 +0100

    upgrade some old deps re vulnerabilities

--- project.clj
+++ project.clj
@@ -7,14 +7,14 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/core.memoize "0.7.1"]
                  [ovotech/abracad "0.4.16"]
                  [cheshire "5.10.0"]
 
-                 [clj-http "3.11.0"]
+                 [clj-http "3.12.0"]
 
-                 [org.apache.kafka/kafka-clients "2.1.0"
+                 [org.apache.kafka/kafka-clients "2.4.1"
                   :exclusions [org.scala-lang/scala-library]]
                  [org.clojure/tools.logging "0.4.1"]]
 
@@ -25,7 +25,7 @@
 
   :repositories {"confluent" "https://packages.confluent.io/maven"}
 
-  :profiles {:dev {:dependencies   [[vise890/zookareg "2.1.0-1"]
+  :profiles {:dev {:dependencies   [[vise890/zookareg "2.4.1-1"]
                                     [ch.qos.logback/logback-classic "1.2.3"]
                                     [ch.qos.logback/logback-core "1.2.3"]]
                    :resource-paths ["dev/resources" "test/resources"]}
